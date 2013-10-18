030fe0010f4f4245f4cc3a9ad1a5df6bc42f7e6c
Chris <git.simpkins@gmail.com>
2013-10-18 16:13:14 -0400

    updated makeinstall.sh script comments

--- makeinstall.sh
+++ makeinstall.sh
@@ -7,7 +7,9 @@
 #		- sixfour.py
 #		- setup.py
 # 3 > Run this file
-#    - creates sixfour 'binary' script
+#    - creates sixfour 'binary' script for user to run at CL
+# 4 > Upload to PyPi
+#    - python setup.py upload
 
 # create the main script that will run from the command line
 cp sixfour.py sixfour
