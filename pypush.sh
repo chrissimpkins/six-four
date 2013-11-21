8b83278e22b4af2121ca1d326caa8bb39b3204a1
Chris <git.simpkins@gmail.com>
2013-11-21 08:24:08 -0500

    new deployment shell script

---
+++ pypush.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Scriptacular - pypush.sh
+# Create a Python source distribution and push it to PyPi
+# Copyright 2013 Christopher Simpkins
+# MIT License
+
+
+# Build and push to PyPi
+python setup.py sdist upload
+
+# Confirm that it worked
+if (( $? )); then
+  echo "Unable to distribute your release to PyPi" >&2
+  exit 1
+fi
+
+python setup.py bdist_wheel upload
+
+# Confirm that wheel distribution worked
+if (( $? )); then
+  echo "Unable to distribute your wheel to PyPi" >&2
+  exit 1
+fi
+
+# Exit success
+exit 0
